300cfd6098629561566ca4aa44899c8eea6eeb6b
kg9dq <kg9dq@virginia.edu>
2014-03-20 18:51:53 -0400

    NPC Entity

--- Hide Out/Hide Out/Entities/Obstacle.cs
+++ Hide Out/Hide Out/Entities/Obstacle.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Hide_Out.Entities
 {
@@ -10,6 +12,37 @@ namespace Hide_Out.Entities
     {
         Boolean CanOverlapWith { get; set; }
         Boolean CanSeeThrough { get; set; }
-        ObstacleType Tag { get; set; }
+        ObstacleType tag { get; set; }
+
+        public Obstacle(ObstacleType type, Vector2 pos, Texture2D spr)
+        {
+            position = pos;
+            sprite = spr;
+            tag = type;
+            switch (type)
+            {
+                case ObstacleType.Bush:
+                    CanOverlapWith = true;
+                    CanSeeThrough = false;
+                    break;
+                case ObstacleType.Tree:
+                    CanOverlapWith = false;
+                    CanSeeThrough = false;
+                    break;
+                case ObstacleType.Fountain:
+                    CanOverlapWith = false;
+                    CanSeeThrough = true;
+                    break;
+                case ObstacleType.Pond:
+                    CanOverlapWith = false;
+                    CanSeeThrough = true;
+                    break;
+            }
+        }
+
+        public Obstacle(Texture2D spr)
+            : this(ObstacleType.Bush, new Vector2(0, 0), spr)
+        {
+        }
     }
 }

8356fe1998a54d24187c90f19b7cf0b5a4b4d03b
SydneyH <slh4sy@virginia.edu>
2014-03-20 19:28:10 -0400

    added Item, ItemController methods

--- Hide Out/Hide Out/Controllers/ItemController.cs
+++ Hide Out/Hide Out/Controllers/ItemController.cs
@@ -3,9 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Hide_Out.Entities;
+
 namespace Hide_Out.Controllers
 {
     class ItemController
     {
+
+        public List<Item> activeItems { get; set; }
+
+        public ItemController()
+        {
+            activeItems = new List<Item>();
+        }
+
+        public void addItem(Item item)
+        {
+            activeItems.Add(item);
+        }
+
+        public void removeItem(Item item)
+        {
+            activeItems.Remove(item);
+        }
+
+        public void clearItems()
+        {
+            activeItems.Clear();
+        }
+
+        public void updateItems(int CurrentTime)
+        {
+            foreach (Item item in this.activeItems){
+                item.updateTime(CurrentTime);
+                if (item.expirationTime >= item.time)
+                {
+                    activeItems.Remove(item);
+                }
+            }
+        }
+
+        public void pickUp(Item item)
+        {
+            activeItems.Remove(item);
+        }
     }
 }
--- Hide Out/Hide Out/Entities/Item.cs
+++ Hide Out/Hide Out/Entities/Item.cs
@@ -2,12 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Hide_Out.Entities
 {
     enum ItemType { WaterBottle, CandyBar }; 
     class Item : Entity
     {
-        ItemType Tag { get; set; }
+        public ItemType tag { get; set; }
+        public bool canPickUp { get; set; }
+        public int expirationTime { get; set; }
+        public int time { get; set; }
+        public bool isVisible { get; set; }
+
+        public Item(ItemType type, Vector2 pos, Texture2D spr, int lifeTime, bool visible, int currentTime)
+        {
+            position = pos;
+            sprite = spr;
+            tag = type;
+            time = currentTime;
+            expirationTime = time + lifeTime;
+            isVisible = visible;
+            canPickUp = true;
+            sprite = spr;
+        }
+
+        public void pickUp()
+        {
+            canPickUp = false;
+            isVisible = false;
+        }
+
+        public void updateTime(int currentTime)
+        {
+            time = time + (currentTime - time);
+        }
+
     }
 }

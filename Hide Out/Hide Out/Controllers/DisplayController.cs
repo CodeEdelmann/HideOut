c6e7c03c365e695518938e7c78b492f650382c4e
rjb3vp <1robot4U>
2014-04-03 17:18:53 -0400

    fixing csproj

--- Hide Out/Hide Out/Controllers/DisplayController.cs
+++ Hide Out/Hide Out/Controllers/DisplayController.cs
@@ -13,11 +13,37 @@ namespace HideOut.Controllers
 {
     class DisplayController
     {
+
+        int xPosition = 0;
+        int yPosition = 100;
+        int xSize = 8;
+        int ySize = 12;
+        int yOffset = 2;
+        int xOffset = 4;
+
+
        //  public static readonly int SPRITE_SIZE = 50;
         List<Display> displays;
             
         private Texture2D fontTexture;
+
+        private Texture2D loseGameTexture;
+        private Texture2D hungerTexture;
+        private Texture2D thirstTexture;
+        private Texture2D winGameTexture;
         Rectangle sizingRectangle;
+        public bool hasLost = false;
+        public bool hasWon = false;
+
+        public void lose()
+        {
+            hasLost = true;
+        }
+        public void win()
+        {
+            hasWon = true;
+
+        }
         public DisplayController()
         {
 
@@ -33,7 +59,22 @@ namespace HideOut.Controllers
    
         }
 
-  
+
+        public void drawStats(int hunger, int thirst, SpriteBatch sb)
+        {
+            for (int i = 0; i < hunger; i++)
+            {
+                sb.Draw(hungerTexture, new Rectangle(20 * i, 0, 20, 20), Color.White);
+            }
+
+            for (int i = 0; i < thirst; i++)
+            {
+                sb.Draw(thirstTexture, new Rectangle(20 * i, 50, 20, 20), Color.White);
+            }
+
+
+        }
+
         public void Draw(SpriteBatch sb)
         {
             //sb.Draw(thePlayer.sprite, thePlayer.screenRectangle, Color.White);
@@ -42,78 +83,105 @@ namespace HideOut.Controllers
 
             //sb.Draw(thePlayer.sprite, thePlayer.screenRectangle, source, Color.White);
            // sb.Draw(fontTexture, source, sizingRectangle, Color.White);
-            int xPosition = 0;
-            int yPosition = 100;
-            int xSize = 8;
-            int ySize = 12;
-            int yOffset = 2;
-            int xOffset = 4;
+         
             //sb.Draw(fontTexture, new Rectangle(xPosition, yPosition, 211, 40), sizingRectangle, Color.White);
-            
-            
-            
+
+ 
+
+
+
+            xPosition = 0;
+             yPosition = 100;
+            xSize = 8;
+             ySize = 12;
+             yOffset = 2;
+             xOffset = 4;
+
+
+             if (hasLost)
+             {
+                 sb.Draw(loseGameTexture, new Rectangle(0, 0, 100, 100), Color.White);
+                 displayString("Game Over", 0, 0, sb);
+             }
+             else
+             {
+                 if (hasWon)
+                 {
+                     sb.Draw(winGameTexture, new Rectangle(0, 0, 100, 100), Color.White);
+                     displayString("Congratulations!", 0, 0, sb);
+                 }
+             }
+
+
+
             //good for testing
             //sb.Draw(fontTexture, new Rectangle(xPosition, yPosition, xSize, ySize), new Rectangle(2 + (xSize * xOffset), 1 + (ySize * yOffset), xSize, ySize), Color.White);
 
             for (int i = 0; i < displays.Count(); i++)
             {
-                for (int ii = 0; ii < displays[i].text.Length; ii++)
-                {
-                    String workingString = displays[i].text.Substring(ii, 1);
-                    //workingString.First().
-                    char workingChar = displays[i].text[ii];
+                String currentString = displays[i].text;
+                displayString(currentString, displays[i].x, displays[i].y, sb);
+                
+            }
 
-                    //double result = char.GetNumericValue(displays[i].text, ii);
-                   
-                    //Console.WriteLine("Result: " + result);
-                    if (workingChar == ' ')
+        }
+        public void displayString(String currentString, int xPos, int yPos, SpriteBatch sb)
+        {
+
+            for (int ii = 0; ii < currentString.Length; ii++)
+            {
+                String workingString = currentString.Substring(ii, 1);
+                //workingString.First().
+                char workingChar = currentString[ii];
+
+                //double result = char.GetNumericValue(displays[i].text, ii);
+
+                //Console.WriteLine("Result: " + result);
+                if (workingChar == ' ')
+                {
+                    //do nothing, this is a gap
+
+
+                }
+                else
+                {
+                    if (Char.IsLetter(workingChar))
                     {
-                        //do nothing, this is a gap
+                        int result = char.ToUpper(workingChar) - 64;
+                        xPosition = xPos + (xSize * ii);
+                        yPosition = yPos;
+                        yOffset = 1;//hard coded as caps right now
+                        xOffset = result - 1;//for letters
+
+                        sb.Draw(fontTexture, new Rectangle(xPosition, yPosition, xSize, ySize), new Rectangle(2 + (xSize * xOffset), 1 + (ySize * yOffset), xSize, ySize), Color.White);
 
 
                     }
                     else
                     {
-                        if (Char.IsLetter(workingChar))
+                        if (Char.IsNumber(workingChar))
                         {
-                            int result = char.ToUpper(workingChar) - 64;
-                            xPosition = displays[i].x + (xSize * ii);
-                            yPosition = displays[i].y;
-                            yOffset = 1;//hard coded as caps right now
-                            xOffset = result - 1;//for letters
+                            int result = Convert.ToInt32(workingString);
+                            if (result > -1)//TODO: handle negatives?
+                            {
+                                xPosition = xPos + (xSize * ii);
+                                yPosition = yPos;
+                                yOffset = 2;//numbers
+                                xOffset = result;//for numbers
 
-                            sb.Draw(fontTexture, new Rectangle(xPosition, yPosition, xSize, ySize), new Rectangle(2 + (xSize * xOffset), 1 + (ySize * yOffset), xSize, ySize), Color.White);
-
-
+                                sb.Draw(fontTexture, new Rectangle(xPosition, yPosition, xSize, ySize), new Rectangle(2 + (xSize * xOffset), 1 + (ySize * yOffset), xSize, ySize), Color.White);
+                            }
                         }
                         else
                         {
-                            if (Char.IsNumber(workingChar))
-                            {
-                                int result = Convert.ToInt32(workingString);
-                                if (result > -1)//TODO: handle negatives?
-                                {
-                                    xPosition = displays[i].x + (xSize * ii);
-                                    yPosition = displays[i].y;
-                                    yOffset = 2;//numbers
-                                    xOffset = result;//for numbers
+                            //handle symbols, punctuations, other special cases here
 
-                                    sb.Draw(fontTexture, new Rectangle(xPosition, yPosition, xSize, ySize), new Rectangle(2 + (xSize * xOffset), 1 + (ySize * yOffset), xSize, ySize), Color.White);
-                                }
-                            }
-                            else
-                            {
-                                //handle symbols, punctuations, other special cases here
-
-                            }
                         }
                     }
-
                 }
+
             }
-
         }
-
         public void addDisplay(int xx, int yy, String text)
         {
             displays.Add(new Display(xx, yy, text));
@@ -124,9 +192,20 @@ namespace HideOut.Controllers
             //Start by loading all textures
             //playerTexture = cm.Load<Texture2D>("player.png");
             fontTexture = cm.Load<Texture2D>("basicFont.png");
+
+            winGameTexture = cm.Load<Texture2D>("victory.png");
+
+           loseGameTexture = cm.Load<Texture2D>("gameOver.png");
+
+
+           thirstTexture = cm.Load<Texture2D>("waterBottle.png");
+
+           hungerTexture = cm.Load<Texture2D>("apple.png");
+
+
             sizingRectangle = new Rectangle(0, 0, 211, 40);
             displays = new List<Display>();
-
+            //addDisplay(0, 0,
 
 
             //testing

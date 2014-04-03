33fbc88c208a3ce244ac9dd1885ada39b7b073f4
kg9dq <kg9dq@virginia.edu>
2014-04-03 17:21:09 -0400

    Level 2

--- Hide Out/Hide Out/Controllers/LevelController.cs
+++ Hide Out/Hide Out/Controllers/LevelController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace HideOut.Controllers
 {
